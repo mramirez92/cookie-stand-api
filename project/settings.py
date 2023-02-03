8dd3891cd3030352f46a23bc1bb926cbf0c6f2d9
Monica <ramirez.92.monica@gmail.com>
2023-02-02 19:41:14 -0600

    model set up

--- project/settings.py
+++ project/settings.py
@@ -61,7 +61,8 @@ INSTALLED_APPS = [
     "corsheaders",
     # local
     "accounts",
-    "things",
+    # "things",
+    "cookie_stands",
 ]
 
 MIDDLEWARE = [
--- things/admin.py
+++ things/admin.py
@@ -1,5 +1,5 @@
-from django.contrib import admin
-from .models import Thing
-
-# Register your models here.
-admin.site.register(Thing)
+# from django.contrib import admin
+# from .models import Thing
+#
+# # Register your models here.
+# admin.site.register(Thing)
