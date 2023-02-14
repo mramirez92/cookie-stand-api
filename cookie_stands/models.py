ed53a17972b8e8079b7e7677a41d7b68901fd29f
mramirez92 <ramirez.92.monica@gmail.com>
2023-02-13 22:51:15 -0600

    mod to models, list compreshension for hourly_sales

--- cookie_stands/models.py
+++ cookie_stands/models.py
@@ -1,8 +1,8 @@
+import random
 from django.db import models
 from django.contrib.auth import get_user_model
 
 
-# Create your models here.
 class CookieStand(models.Model):
     location = models.CharField(max_length=256)
     owner = models.ForeignKey(
@@ -16,3 +16,16 @@ class CookieStand(models.Model):
 
     def __str__(self):
         return self.location
+
+    def save(self, *args, **kwargs):
+        if not self.pk and not self.hourly_sales:
+            min = self.minimum_customers_per_hour
+            max = self.maximum_customers_per_hour
+
+            cookies_each_hour = [
+                int(random.randint(min, max) * self.average_cookies_per_sale)
+                for _ in range(14)]
+
+            self.hourly_sales = cookies_each_hour
+
+        super().save(*args, **kwargs)
